ce501693ca0b560e612982c1d7217cc7320bfc75
giliyoffe <gili26yoffe@gmail.com>
2018-02-09 14:40:52 +0100

    Pages folder and news page

---
+++ src/pages/News.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Header from '.././layout/Header.js';
+
+
+class News extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+
+          <h1>IN todays News</h1>
+
+        </div>
+
+    );
+  }
+}
+
+export default News;
