71550238ba5072fb24456dfd025c63102da92514
giliyoffe <gili26yoffe@gmail.com>
2018-02-09 14:38:32 +0100

    Added Footer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import Layout from './layout/Layout.js';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-        </header>
-        <Layout />
-    </div>
+		<div className="App">
+		  <header className="App-header"></header>
+		  <Layout />
+		</div>
     );
   }
 }
