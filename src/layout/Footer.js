a951b04a027a160463a49f52bd829149c6354d4d
giliyoffe <gili26yoffe@gmail.com>
2018-02-09 14:42:32 +0100

    added Footer file

---
+++ src/layout/Footer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <div className="App-footer">
+
+        <p> &copy;  G-Corp</p>
+             
+      </div>
+    );
+  }
+}
+
+export default Footer;
