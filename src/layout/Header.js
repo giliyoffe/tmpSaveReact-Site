80aa11f125471e24580fb9167524f7e4ecb9dfb4
giliyoffe <gili26yoffe@gmail.com>
2018-02-09 14:44:24 +0100

    adding news page link

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import News from '.././pages/News.js';
+
 
 class Header extends React.Component {
   render() {
     return (
       <div className="App-header">
-        <h1> Header </h1>
+        <h1> Website Title </h1>
+
+        <div className="topnav">
+            <a className="active" href="#home">Home</a>
+            <a href={News}>News</a>
+            <a href="#contact">Contact</a>
+            <a href="#about">About</a>
+        </div>
       </div>
     );
   }
